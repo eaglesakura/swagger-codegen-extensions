7a067fedbac96b8a6695e43eba5bb124ea872bef
eaglesakura <eagle.sakura@gmail.com>
2023-10-02 13:17:40 +0900

    fix: 入れ子のModel名が適切に出力されない問題を修正 refs #3

--- swagger2-codegen/src/main/kotlin/com/eaglesakura/swagger2/extensions/SwaggerExtensions.kt
+++ swagger2-codegen/src/main/kotlin/com/eaglesakura/swagger2/extensions/SwaggerExtensions.kt
@@ -2,6 +2,33 @@ package com.eaglesakura.swagger2.extensions
 
 import io.swagger.models.Swagger
 import io.swagger.models.parameters.BodyParameter
+import io.swagger.models.properties.ObjectProperty
+import io.swagger.models.properties.Property
+
+private fun normalizePathTitle(s: String): String {
+    return s.replace("{", "")
+        .replace("}", "")
+        .replace("/", "_")
+        .replace("__", "_")
+        .toLowerCase()
+        .let {
+            if (it.startsWith("_")) {
+                it.substring(1)
+            } else {
+                it
+            }
+        }
+}
+
+private fun normalizeProperties(prefix: String, properties: Map<String, Property>) {
+    properties
+        .filterValues { property -> property is ObjectProperty }
+        .forEach { (name, prop) ->
+            val obj = prop as ObjectProperty
+            obj.title = normalizePathTitle("${prefix}_$name")
+            normalizeProperties(obj.title, obj.properties)
+        }
+}
 
 /**
  * Update Swagger data.
@@ -11,35 +38,21 @@ fun Swagger.normalize() {
         pathModel.operationMap.forEach { (method, op) ->
             op.parameters
                 .filterIsInstance(BodyParameter::class.java)
-                .filter { it.schema?.properties?.isNotEmpty() == true }
                 .forEach { param ->
-                    param.schema.title = "${path}_${method}_${param.name}"
-                        .replace("/", "_")
-                        .replace("__", "_")
-                        .toLowerCase()
-                        .let {
-                            if (it.startsWith("_")) {
-                                it.substring(1)
-                            } else {
-                                it
-                            }
-                        }
+                    val schema = param.schema ?: return@forEach
+                    schema.title = normalizePathTitle("${path}_${method}_${param.name}")
+
+                    val properties = schema.properties ?: return@forEach
+                    normalizeProperties(schema.title, properties)
                 }
-            op.responses.forEach { (status, resp) ->
-                if (resp.responseSchema?.properties?.isNotEmpty() == true) {
-                    resp.responseSchema.title = "${path}_${method}_$status"
-                        .replace("/", "_")
-                        .replace("__", "_")
-                        .toLowerCase()
-                        .let {
-                            if (it.startsWith("_")) {
-                                it.substring(1)
-                            } else {
-                                it
-                            }
-                        }
+            op.responses
+                .forEach { (status, resp) ->
+                    val responseSchema = resp.responseSchema ?: return@forEach
+                    responseSchema.title = normalizePathTitle("${path}_${method}_$status")
+
+                    val properties = responseSchema.properties ?: return@forEach
+                    normalizeProperties(responseSchema.title, properties)
                 }
-            }
         }
     }
 }
